5439356d621bbb9b3ec3014f7e33df09ba5a6e85
Anton Kiselev <anton.kisel@gmail.com>
2021-07-21 19:32:58 +0000

    update telnet to nodemcu 3.x

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ if file.exists('lconfig.lua') then
   dofile("lconfig.lua")
 end
 dofile("wifi.lua")
-dofile("telnet.lua")
+dofile("telnet.lua"):open()
 if file.exists('app.lua') then
   dofile("app.lua")
 end
--- telnet.lua
+++ telnet.lua
@@ -1,33 +1,58 @@
-telnet_srv = net.createServer(net.TCP, 180)
-telnet_srv:listen(23, function(socket)
-    local fifo = {}
-    local fifo_drained = true
+local M = {}
+local modname = ...
+local function telnet_session(socket)
+  local node = node
+  local stdout
 
-    local function sender(c)
-        if #fifo > 0 then
-            c:send(table.remove(fifo, 1))
-        else
-            fifo_drained = true
-        end
+  local function output_CB(opipe)   -- upval: socket
+    stdout = opipe
+    local rec = opipe:read(1400)
+    if rec and #rec > 0 then socket:send(rec) end
+    return false -- don't repost as the on:sent will do this
+  end
+
+  local function onsent_CB(skt)     -- upval: stdout
+    local rec = stdout:read(1400)
+    if rec and #rec > 0 then skt:send(rec) end
+  end
+
+  local function disconnect_CB(skt) -- upval: socket, stdout
+    node.output()
+    socket, stdout = nil, nil -- set upvals to nl to allow GC
+  end
+
+  node.output(output_CB, 0)
+  socket:on("receive", function(_,rec) node.input(rec) end)
+  socket:on("sent", onsent_CB)
+  socket:on("disconnection", disconnect_CB)
+  print(("Welcome to NodeMCU world (%d mem free, %s)"):format(
+        node.heap(), wifi.sta.getip()))
+end
+
+function M.open(this, ssid, pwd, port)
+  local tmr, wifi, uwrite = tmr, wifi, uart.write
+  if ssid then
+    wifi.setmode(wifi.STATION, false)
+    wifi.sta.config { ssid = ssid, pwd  = pwd, save = false }
+  end
+  local t = tmr.create()
+  t:alarm(500, tmr.ALARM_AUTO, function()
+    if (wifi.sta.status() == wifi.STA_GOTIP) then
+      t:unregister()
+      t=nil
+      print(("Telnet server started (%d mem free, %s)"):format(
+             node.heap(), wifi.sta.getip()))
+      M.svr = net.createServer(net.TCP, 180)
+      M.svr:listen(port or 23, telnet_session)
+    else
+      uwrite(0,".")
     end
+  end)
+end
 
-    local function s_output(str)
-        table.insert(fifo, str)
-        if socket ~= nil and fifo_drained then
-            fifo_drained = false
-            sender(socket)
-        end
-    end
+function M.close(this)
+  if this.svr then this.svr:close() end
+  package.loaded[modname] = nil
+end
 
-    node.output(s_output, 0)
-    socket:on("receive", function(c, l)
-        node.input(l)
-    end)
-    socket:on("disconnection", function(c)
-        node.output(nil)
-    end)
-    socket:on("sent", sender)
-
-    print("Welcome to NodeMCU world.")
-end)
-
+return M
