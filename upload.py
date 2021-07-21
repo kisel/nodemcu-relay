36ed8f30be7ac301782bf7253a97b93a9b5d032b
Anton Kiselev <anton.kisel@gmail.com>
2021-07-21 09:40:31 +0000

    upload.py: --keep option

--- upload.py
+++ upload.py
@@ -3,19 +3,20 @@ import argparse
 import asyncio
 import time
 import binascii
+import re
+RE_COMMENT = '^\s*--.*'
 
 async def main():
     parser = argparse.ArgumentParser(description='Uploads script to nodemcu via telnet connection')
     parser.add_argument('host', help='nodemcu hostname/ip')
     parser.add_argument('--port', default=23, help='telnet port')
     parser.add_argument('--debug', action='store_true')
+    parser.add_argument('--keep', action='store_true', help='keep formatting, comments')
     parser.add_argument('file', type=str, nargs="+", help='files to upload')
 
     args = parser.parse_args()
 
     reader, writer = await asyncio.open_connection(args.host, args.port)
-    def wait_prompt():
-        time.sleep(0.1)
 
     async def send(cmd):
         writer.write(cmd.encode())
@@ -23,9 +24,9 @@ async def main():
             print(cmd)
         await writer.drain()
         res = await reader.readuntil(b">")
+        # give 1ms to send extra > or ' ', '\n' if any
         time.sleep(0.01)
-        b = await reader.read(1024)
-        res += b
+        res += await reader.read(1024)
         if args.debug:
             print("recv prompt: " + res.decode())
         else:
@@ -39,11 +40,12 @@ async def main():
         await send("function unhex(str) return (str:gsub('..', function (cc) return string.char(tonumber(cc, 16)) end)) end")
 
         for l in open(fn):
+            if not args.keep and re.match(RE_COMMENT, l):
+                next
             if args.debug:
                 print('sending: ' + l.strip())
             await send("f:write(unhex('{}'))\n".format(binascii.hexlify(l.encode()).decode('ascii')))
             await writer.drain()
-            wait_prompt()
 
         await send("f:close()")
         print("\nsent " + fn)
